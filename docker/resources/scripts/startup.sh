3decb71c583be082b30b1518df9744a30ed71481
Oriol Boixx <oboix@ilg.cat>
2023-03-24 17:30:00 +0000

    fixed ln

--- docker/resources/scripts/startup.sh
+++ docker/resources/scripts/startup.sh
@@ -4,5 +4,6 @@
     #mkdir -p /home/$NB_USER/.local/share/code-server/extensions
     #rm -rf /home/$NB_USER/.vscode/extensions
     #ln -s /home/$NB_USER/.local/share/code-server/extensions /home/$NB_USER/.vscode/
+    mkdir -p /usr/share/jupyter
     ln -s /opt/conda/share/jupyter/kernels  /usr/share/jupyter/kernels
     service ssh start
