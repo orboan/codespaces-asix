5b4f9c902a1d92b44055aece85e67dd0d3cb3124
Oriol Boixx <oboix@ilg.cat>
2023-03-24 18:12:47 +0000

    first commit

--- demo/src/main/java/com/example/model/Student.java
+++
@@ -1,8 +0,0 @@
-package com.example.model;
-
-public class Student {
-    @Override
-    public String toString(){
-        return "I am a student";
-    }
-}
--- docker/resources/docker/compose/mysql/initdb/fix_user.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-  
-user=$NB_USER
-
-mysql -u root -padmin -e "CREATE USER '$user'@'%' IDENTIFIED BY 'pluralcamp'; GRANT ALL PRIVILEGES ON *.* TO '$user'@'%'; FLUSH PRIVILEGES;";
